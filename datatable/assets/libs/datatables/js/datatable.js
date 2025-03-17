e1b76bf5b815cacd5f1d3d15a469007324507ff5
Codexsafe <codexsafe@gmail.com>
2025-03-17 11:25:13 +0530

    Update datatable.js

--- datatable/assets/libs/datatables/js/datatable.js
+++ datatable/assets/libs/datatables/js/datatable.js
@@ -1,139 +1,59 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let colors = ["#727cf5", "#0acf97", "#fa5c7c", "#6c757d", "#39afd1"];
-    // Bar Chart (Chart1)cls
-    let barOptions = {
-        chart: {
-            height: 380, 
-            type: "bar"
-        },
-        series: [{
-            name: "Error Records",
-            data: []
-        }],
-        xaxis: {
-            categories: []
-        },
-        colors: colors,
-        plotOptions: {
-            bar: {
-                horizontal: false,
-                columnWidth: "60%"
-            }
-        },
-        tooltip: {
-            y: {
-                formatter: function(val) {
-                    return val + " Errors";
-                }
-            }
-        }
-    };
-    let barChart = new ApexCharts(document.querySelector("#chart1"), barOptions);
-    barChart.render();
-    function updateChart(system) {
-        let dataMap = {
-            "meter": """+str(meter)+""",
-            "solar": """+str(solar)+""",
-            "ev": """+str(ev)+""",
-            "essentials": """+str(essentials)+"""
-        };
-        let categories = """+str(cols)+""";
-        barChart.updateOptions({
-            xaxis: { categories: categories }
-        });
-        barChart.updateSeries([{
-            name: "Error Records",
-            data: dataMap[system]
-        }]);
-    }
-
-    document.getElementById("meter").addEventListener("click", function() {
-        updateChart("meter");
-    });
-    document.getElementById("solar").addEventListener("click", function() {
-        updateChart("solar");
-    });
-    document.getElementById("ev").addEventListener("click", function() {
-        updateChart("ev");
-    });
-    document.getElementById("essentials").addEventListener("click", function() {
-        updateChart("essentials");
-    });
-    updateChart("meter");
-
-    // Donut Chart (Chart2)
-    let donutOptions = {
-        chart: {
-            height: 380, 
-            type: "donut"
-        },
-        series: [
-            """+str(sum(meter))+""", // Meter Management
-            """+str(sum(solar))+""", // Solar
-            """+str(sum(ev))+""", // EV
-            """+str(sum(essentials))+"""  // Essentials
+$(document).ready(function() {
+    "use strict";
+    $("#reportTable").DataTable({
+        responsive: true,
+        pagingType: "full_numbers",
+        order: [],
+        autoWidth: false, 
+        aoColumns : [
+            { sWidth: '15%' },
+            { sWidth: '20%' },
+            { sWidth: '20%' },
+            { sWidth: '20%' },
+            { sWidth: '30%' }
         ],
-        labels: ["Meter Management", "Solar", "EV", "Essentials"],
-        colors: colors,
-        dataLabels: { enabled: false },
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                chart: {
-                    height: 380
-                },
-                legend: { show: false }
+        dom: '<"top d-flex justify-content-between"<"dt-left-col d-flex align-items-center"l><"dt-right-group d-flex"Bf>>rt<"bottom d-flex justify-content-between"<"dt-info d-flex align-items-center"i><"dt-pagination"p>>',
+        buttons: [
+            {
+                extend: 'collection',
+                text: '<span class="mdi mdi-export" style="color:white !important;"> Export</span>',
+                autoClose: true,
+                buttons: [
+                    { text: '<span class="mdi mdi-content-copy" style="color: gray;"> Copy</span>', extend: 'copyHtml5'},
+                    { text: '<span class="mdi mdi-file-excel-outline" style="color: green;"></span><span style="color: gray;"> Excel</span>', extend: 'excelHtml5'},
+                    { text: '<span class="mdi mdi-file-document-outline" style="color: orange;"></span><span style="color: gray;"> Csv</span>', extend: 'csvHtml5'},
+                    { text: '<span class="mdi mdi-file-pdf-box" style="color: red;"></span><span style="color: gray;"> Pdf</span>', extend: 'pdfHtml5'},
+                    {
+                        text: '<span class="mdi mdi-code-json" style="color: blue;"></span><span style="color: gray;"> Json</span>',
+                        className: 'export-btn',
+                        action: function (e, dt, button, config) {
+                            var data = dt.buttons.exportData();
+                            var blob = new Blob([JSON.stringify(data)], { type: "application/json;charset=utf-8;" });
+                            saveAs(blob, 'Yardi Report.json');
+                        }
+                    },
+                    { text: '<span class="mdi mdi-printer" style="color: purple;"></span><span style="color: gray;"> Print</span>', extend: 'print', className: 'export-btn'}
+                ],
+                fade: false,
+            },
+            {
+                extend: 'colvis',
+                text: '<span class="mdi mdi-eye" style="color: white;"></span><span style="color: white;"> Show</span>',
+                className: 'colvis-btn'
             }
-        }],
-        legend: {
-            position: 'right',
-            offsetY: 0,
-            height: 230,
+        ],
+        language: {
+            "info": "Showing _START_ to _END_ of _TOTAL_ Records",
+            "lengthMenu": "Show _MENU_ Records",
+            "search": "",
+            "infoEmpty": "Showing 0 to 0 of 0 Records",
+            "infoFiltered": "(filtered from _MAX_ total Records)",
+            "zeroRecords": "No matching Records found",
+            "loadingRecords": "Loading Records...",
+            searchPlaceholder: 'Search...'
         },
-        tooltip: {
-            y: {
-                formatter: function(val) {
-                    return val + " Records";
-                }
-            }
-        },
-        plotOptions: {
-            pie: {
-                donut: {
-                    size: "70%"
-                }
-            }
+        drawCallback: function() {
+            $(".dataTables_paginate > .pagination").addClass("pagination-rounded")
         }
-    };
-
-    let donutChart = new ApexCharts(document.querySelector("#donut-chart"), donutOptions);
-    donutChart.render();
-});
-
-// Function to update the active button
-function setActiveButton(buttonId) {
-    var buttons = document.querySelectorAll("button");
-    buttons.forEach(function (button) {
-        button.classList.remove("active");
     });
-    if (buttonId) {
-        document.querySelector(buttonId).classList.add("active");
-    }
-}
-
-// Event listeners for buttons
-document.querySelector("#meter").addEventListener("click", function() {
-    setActiveButton("#meter");
 });
-
-document.querySelector("#solar").addEventListener("click", function() {
-    setActiveButton("#solar");
-});
-
-document.querySelector("#ev").addEventListener("click", function() {
-    setActiveButton("#ev");
-}); 
-
-document.querySelector("#essentials").addEventListener("click", function() {
-    setActiveButton("#essentials");
-}); 
